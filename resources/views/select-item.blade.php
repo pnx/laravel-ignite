c536b67ba6921470bbbcc988d3c8c0d1d1bc3849
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-01-02 09:45:55 +0100

    resources/views/select-item.blade.php: switch hover color to standard tailwind.

--- resources/views/select-item.blade.php
+++ resources/views/select-item.blade.php
@@ -2,7 +2,7 @@
 <li x-on:click="{{ $isPlaceholder ? 'selectPlaceholder()' : "select('$value', '$display')" }}"
     data-display="{{ $display }}"
     data-value="{{ $value }}"
-    class="px-4 py-2 cursor-pointer first:rounded-t last:rounded-b hover:bg-primary hover:text-white">
+    class="px-4 py-2 cursor-pointer first:rounded-t last:rounded-b hover:bg-blue-400 hover:text-white">
 
     @if ($isPlaceholder)
     <span class="text-gray-600">{{ $display }}</span>
