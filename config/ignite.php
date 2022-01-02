58646e3da60f6492b3dd0d00dd151d5b1021f4bb
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-01-02 09:34:42 +0100

    Ádding config/ignite.php

---
+++ config/ignite.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Select component
+	|--------------------------------------------------------------------------
+	|
+	| Config values for the select component
+	|
+	*/
+
+	'select' => [
+
+		/*
+	    |--------------------------------------------------------------------------
+	    | Icon
+	    |--------------------------------------------------------------------------
+	    |
+		| Icon to show to indicate that this field is a dropdown.
+		| The package must blade-ui-kit/blade-icons must be installed as this
+		| string is passed to that package. If undefined no icon will be shown.
+	    |
+	    */
+
+		// 'icon' => 'heroicon-s-chevron-down',
+
+	],
+
+];
