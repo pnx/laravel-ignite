b0f5b9e396a8e45f5f9496ac4233cec74ddbf452
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-04-13 14:16:33 +0200

    tests/TestCase.php: use the InteractsWithViews trait.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,13 @@
 namespace Tests;
 
 use Ignite\FormServiceProvider;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    use InteractsWithViews;
+    
     protected function getPackageProviders($app): array
     {
         return [FormServiceProvider::class];
