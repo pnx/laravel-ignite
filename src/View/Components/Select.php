ad8227fe35b8db7e44dd88b4a7bf5489cf78894c
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-04-26 16:41:15 +0200

    src/View/Components/Select.php: in init() call $value->toString() if available.

--- src/View/Components/Select.php
+++ src/View/Components/Select.php
@@ -108,7 +108,13 @@ class Select extends Component
 			// If so. check for wire:model value and set $this->value
 			$value = $this->attributes->wire('model')->value();
 			if ($value) {
-				$this->value = Arr::get($livewire->all(), $value);
+				$value = Arr::get($livewire->all(), $value);
+
+                if (is_object($value) && method_exists($value, 'toString')) {
+                    $this->value = $value->toString();
+                } else {
+                    $this->value = $value;
+                }
 			}
 		}
 
