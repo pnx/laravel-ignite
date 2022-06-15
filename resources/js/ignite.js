e3a5660f07636e4c1d00dcef89d142bdcc8aeff8
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-06-15 15:45:02 +0200

    resources/js/ignite.js: Adding radio component

--- resources/js/ignite.js
+++ resources/js/ignite.js
@@ -48,4 +48,20 @@ const checkbox = (data) => {
     }
 }
 
-export { select_dropdown, checkbox };
+const radio = (data) => {
+    return {
+
+        // Data
+		// ----------------
+        value: data.value ?? null,
+
+        // Methods
+		// ----------------
+        init() { this.$watch('value', v => { this.$refs.input.dispatchEvent(new Event('input')); })},
+        select(value) { this.value = value },
+        isSelected(value) { return value == this.value },
+    }
+}
+
+
+export { select_dropdown, checkbox, radio };
