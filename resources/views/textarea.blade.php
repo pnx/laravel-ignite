e18f84ea51a5828736f958dab6b3afb3f2d5c9d1
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-06-19 21:06:45 +0200

    resources/views/textarea.blade.php: <textarea> elements are sensitive about whitespace. so remove those.

--- resources/views/textarea.blade.php
+++ resources/views/textarea.blade.php
@@ -7,9 +7,7 @@ if ($errors->has($name)) {
 @endphp
 
 <textarea id="{{ $id }}" name="{{ $name }}" {{ $disabled }}
-    {!! $attributes->merge(['class' => "px-3 py-2 w-full min-h-10 text-gray-600 border $border rounded-md focus:ring focus:ring-gray-300 focus:ring-opacity-50"]) !!}>
-    {{ $value }}
-</textarea>
+    {!! $attributes->merge(['class' => "px-3 py-2 w-full min-h-10 text-gray-600 border $border rounded-md focus:ring focus:ring-gray-300 focus:ring-opacity-50"]) !!}>{{ $value }}</textarea>
 
 @error($name)
 <p class="text-red-400 text-sm">{{ $message }}</p>
